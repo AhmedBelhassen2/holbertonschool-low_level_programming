ec43c73bae981ed11270362db38ced6cac0be5a0
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-04 13:57:39 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 in success
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
