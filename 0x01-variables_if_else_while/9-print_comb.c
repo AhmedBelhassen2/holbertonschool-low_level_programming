076139e196d9b2332d4bf6599ae9b6b4ab0a872a
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-04 22:10:35 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = '0' ; c <= '9' ; c++)
+putchar(c);
+if (c != '9')
+putchar(',');
+putchar(' ');
+
+putchar ('\n');
+return (0);
+}
