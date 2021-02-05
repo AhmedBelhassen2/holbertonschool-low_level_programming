868d951544f0b83b0e3b6a778e058750edd5d130
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-05 11:32:36 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,20 @@ int main(void)
 {
 int i = 0;
 int j = 1;
-
-
 for (i = 0; i < 9; i++)
-putchar(i);
-putchar(j);
-for (j = i + 1; i <= 9; j++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
 
-
+if (i != 8)
+{
 putchar(',');
 putchar(' ');
-
-
+}
+}
+}
 putchar('\n');
 return (0);
 }
