4c51c745a90747f8377f698c333f3800f87055e2
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-09 10:44:52 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - add two number
+ * @a: the operand
+ * @b: second operand
+ *
+ * Return: result of the addition
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
