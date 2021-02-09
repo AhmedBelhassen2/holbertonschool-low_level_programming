abcb58392a9d0964b1817fb580a80c22670335e8
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-09 08:30:48 +0000

    holberton.h

--- 0x02-functions_nested_loop/holberton.h
+++ 0x02-functions_nested_loop/holberton.h
@@ -1,5 +1,11 @@
-#include <stdio.h>
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
 void _putchar(char s);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
 #endif
