6b5ce57da9429778f4fc90439313f4159b594715
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-09 08:39:16 +0000

    C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - check the code for Holberton School students.
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
