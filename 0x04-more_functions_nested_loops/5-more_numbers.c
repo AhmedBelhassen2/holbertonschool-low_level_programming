c4b66681f975af96e8cf3e41265f285672540d95
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-11 16:09:46 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
  * more_numbers - Prints the last digit of a number
  *
- * Return: void.
+ *
+ *
  */
+
 void more_numbers(void)
 {
-int i;
-int x;
+int i, j;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
-for (x = 0; x <= 14; x++)
+for (j = 0; j <= 14; j++)
 {
-if (x < 10)
-else
-{
-_putchar(x / 10 + '0');
-_putchar(x % 10 + '0');
+if (j >= 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
-}
-_putchar('\n');
+_putchar ('\n');
 }
 }
