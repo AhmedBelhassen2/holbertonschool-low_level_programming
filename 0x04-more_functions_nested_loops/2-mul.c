f97978c67e89685079382db202eab217d7a135c8
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-14 11:49:19 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+# inclure  " holberton.h "
+/ * *
+* mul - vérifie si c est un chiffre compris entre 0 et 9 ou non
+* @a: premier entier
+* @b: deuxième entier
+* Renvoie: n la valeur de multiplication
+* /
+int  mul ( int a, int b)
+{
+int n;
+n = a * b;
+retour (n);
+}
