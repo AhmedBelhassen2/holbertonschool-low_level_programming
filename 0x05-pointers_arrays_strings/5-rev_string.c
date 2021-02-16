dac76d45d38164573dbcc8f0ec314bf1ea56f817
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-16 09:16:10 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include"holberton.h"
+
+/**
+ * rev_string - Prints a string followed by a new line to stdout
+ * @s: The string to be printed
+ */
+
+void rev_string(char *s)
+  {
+    int l = 0;
+int i; 
+char *begin_ptr, *end_ptr, ch; 
+   begin_ptr = s;
+end_ptr = s; 
+ while (*(s + l) != 0)
+   {
+     l++;
+   }  
+for (i = 0; i < l - 1; i++)
+    { 
+end_ptr++; 
+    } 
+  for (i = 0; i < l / 2; i++) 
+{ 
+ 
+    ch = *end_ptr; 
+    *end_ptr = *begin_ptr; 
+    *begin_ptr = ch; 
+   
+    begin_ptr++; 
+    end_ptr--; 
+} 
+} 
+
