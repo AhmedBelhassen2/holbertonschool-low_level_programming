4523612e7bfddcfa95cedb4e0f3791448a20cff1
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-17 22:28:45 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@ void puts2(char *str)
 {
 int i, y;
 
-for (i = 0; str[i] != '\0'; i=i++)
+for (i = 0; str[i] != '\0'; i++)
 {
       
 }
 i = i - 1;
 for (y = 0; y <= i ;y = y + 2)
-_putchar(str(y));
+_putchar(str[y]);
 _putchar('\n');
 }
