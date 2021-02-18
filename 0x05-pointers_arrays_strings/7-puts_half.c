c3e2816df298286e908993899883d270dc64d5ce
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-18 09:46:15 +0000

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,26 +6,26 @@
  */
 void puts_half(char *str)
 {
-  int s = 0;
-  int i;
+int s = 0;
+int i;
 
-  while (str[s] != 0)
-    {
-      s++;
-    }
-  s = s -1;
-  if (s % 2 == 0)
+while (str[s] != 0)
+{
+s++;
+}
+s = s - 1;
+if (s % 2 == 0)
 
-    for (i = (s / 2) ; i <= s; i++)
-      {
-	_putchar(str[i]);
-      }
-  else 
-    for (i = (s / 2) + 1 ; i <= s; i++)
-      {
-	_putchar(str[i]);
-      }
-  {
-    _putchar('\n');
-  }
+for (i = (s / 2) ; i <= s; i++)
+{
+_putchar(str[i]);
+}
+else 
+for (i = (s / 2) + 1 ; i <= s; i++)
+{
+_putchar(str[i]);
+}
+{
+_putchar('\n');
+}
 }
