a85163b48b066a59fbf53e31cc3ab684fd757f18
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-18 21:51:57 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
+
 /**
- *_strncat - concatenates n bytes from a string to another
- *@dest: destination string
- *@src: source string
- *@n: number of bytes of str to concatenate
- *return: a pointer to the resulting string
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ * Return: a pointer to the resulting string des
  */
 char *_strncat(char *dest, char *src, int n)
 {
