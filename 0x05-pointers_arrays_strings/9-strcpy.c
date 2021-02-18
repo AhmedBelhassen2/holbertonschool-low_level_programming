e1d0224b1ad376a96054ed45153446f618ce736f
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-18 11:40:47 +0000

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,13 +7,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i; 
+int i;
 for (i = 0; src[i] != '\0' ; i++)
 {
 dest[i] = src[i];
 }
 {
-dest[i] = '\0';   
+dest[i] = '\0';
 return (dest);
 }
 }
