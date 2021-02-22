18ea9bc0ca4afd010f284da624cd3fac466c15ca
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-22 22:45:20 +0000

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_strpbrk -  searches a string for any of a set of bytes.
+ *@s: character
+ *@accept: character
+ *Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+  int i, j;
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      for (j = 0; accept[j] != '\0'; j++)
+	if (s[i] == accept[j])
+	  return (&s[i]);
+     
+    }
+  return (0);
+}
