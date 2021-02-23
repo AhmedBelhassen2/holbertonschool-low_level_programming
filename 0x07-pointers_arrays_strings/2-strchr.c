edb7d7e243fe1f5d40b232e69f95a900798cd4dc
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-23 11:04:50 +0000

    rebase

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,12 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
- for (i = 0;s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-  if (s[i] == c)
+if (s[i] == c)
 {
-  return (&(s[i]));
+return (&(s[i]));
 }
 }
- return (&(s[i]));
+return (0);
 }
