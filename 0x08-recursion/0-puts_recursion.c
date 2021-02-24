731d9b21543081b8125434e19f14b10debddd49f
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-24 12:34:31 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,13 @@
  * _puts - Prints a string followed by a new line to stdout
  * @s: The string to be printed
  */
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
-if ( *s == '\0')
+if (*s == '\0')
 {
 _putchar ('\n');
 return;
 }
 _putchar (*s);
-_puts_recursion(s+1);
+_puts_recursion(s + 1);
 }
