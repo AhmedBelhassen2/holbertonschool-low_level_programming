895ddafa5603832ac9b4d1a6e50553b070fca565
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-24 22:22:37 +0000

    holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -4,4 +4,6 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 #endif
