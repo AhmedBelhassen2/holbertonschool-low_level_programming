bb689b964596b4cc866d47c01102d91011397c19
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-28 20:00:03 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main to an integer.
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
