509e6574c745d8018dd7c5756b4b2dc1e1c78689
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-02-28 19:00:11 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - print the name of the prog
  *@argc: argc
  *@argv: argv
  * Return: 0
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[]) 
 {
-while(argc--)
-printf("%s\n", *argv++);
-exit(EXIT_SUCCESS);
+printf(" %s\n",argv[0]);
+return 0;
 }
