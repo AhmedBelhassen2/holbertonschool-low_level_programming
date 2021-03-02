4a739f396957ae17f610339ca981f29ec467fa99
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-02 11:37:10 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 char *n;
 unsigned int j = 0;
-n = malloc(sizeof(char) * size); 
+n = malloc(sizeof(char) * size);
 if (size == 0)
 return (NULL);
 if (n == NULL)
