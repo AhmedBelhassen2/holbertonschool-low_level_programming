36cf39ed6c5a748f18913024841cab04c009bae4
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-04 12:08:05 +0000

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include"holberton"
+#include <stdio.h>
+/**
+* malloc_checked - Allocates memory
+* @b: values
+* Return: A pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b)
+if (p == NULL)
+exit(98);
+return (p);
+}
