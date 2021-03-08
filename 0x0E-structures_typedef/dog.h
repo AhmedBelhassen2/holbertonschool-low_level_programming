e66f8f442c1dd3590c21a4bb0836088b9b56a959
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-08 12:37:56 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Descriptors for dogs
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
