a9e2e35f9ae4ea20be96a1d94b88dfb43b53757c
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-10 22:58:08 +0000

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,8 +6,7 @@
  * main - Do mathematical calculations with arguments passed to program
  * @argc: The number of arguments passed to the program
  * @argv: The pointer to the array of arguments passed to the program
- *
- * Return: Always 0
+ * Return: pointer to corresponding function.
  */
 
 int main(int argc, char **argv)
@@ -17,23 +16,23 @@ int (*f)(int, int);
 int r;
 
 if (argc != 4)
-    {
-      printf("Error\n");
-      exit(98);
-    }
- num1 = atoi(argv[1]);
- num2 = atoi(argv[3]);
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 f = get_op_func(argv[2]);
- if (!f)
-  {
-    printf("Error\n");
-    exit(99);
-  }
- if (num2 == 0 && (f == op_div || f == op_mod))
-   {
-     printf("Error\n");
-     exit(100);
-   }
+if (!f)
+{
+printf("Error\n");
+exit(99);
+}
+if (num2 == 0 && (f == op_div || f == op_mod))
+{
+printf("Error\n");
+exit(100);
+}
 r = f(num1, num2);
 printf("%d\n", r);
 return (0);
