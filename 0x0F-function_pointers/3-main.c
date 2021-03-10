970bd317b8272da49f9737e96cbcd6ead7397c85
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-10 16:28:18 +0000

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Do mathematical calculations with arguments passed to program
+ * @argc: The number of arguments passed to the program
+ * @argv: The pointer to the array of arguments passed to the program
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+int num1 = 0, num2 = 0;
+int (*f)(int, int);
+int r;
+
+if (argc != 4)
+    {
+      printf("Error\n");
+      exit(98);
+    }
+ num1 = atoi(argv[1]);
+ num2 = atoi(argv[3]);
+f = get_op_func(argv[2]);
+ if (!f)
+  {
+    printf("Error\n");
+    exit(99);
+  }
+ if (num2 == 0 && (f == op_div || f == op_mod))
+   {
+     printf("Error\n");
+     exit(100);
+   }
+r = f(num1, num2);
+printf("%d\n", r);
+return (0);
+}
