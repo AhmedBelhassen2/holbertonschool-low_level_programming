9062d9813ad612257747b23edbfbcfaa9b9e861d
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-19 12:27:23 +0000

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,14 +10,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t  *new_node_begin = malloc(sizeof(list_t));
-	unsigned int j;
+	int j = 0;
 
+	while (str[j++])
+		;
 	if (new_node_begin != NULL)
 	{
-		for (j = 0; str[j]; j++)
 
 		new_node_begin->str = strdup(str);
-		new_node_begin->len = j;
+		new_node_begin->len = j - 1;
 		new_node_begin->next = *head;
 		*head = new_node_begin;
 		return (new_node_begin);
