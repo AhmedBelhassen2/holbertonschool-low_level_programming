cb4893c9bb470f5a14094f6689ef2a962680f10a
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-25 17:18:22 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 /**
-*
-*
+* binary_to_uint - converts a binary number to an unsigned int
+* @b: pointer to string
+* Return: the number in base 10
 */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int convert = 0;
+	unsigned int convbase10 = 0;
 	unsigned int countb;
+
 	if (b == NULL)
 		return (0);
 	for (countb = 0; b[countb] != '\0'; countb++)
 	{
 		if (b[countb] < '0' || b[countb] > '1')
 			return (0);
-		convert = 2 * convert + (b[countb] - '0');
+		convbase10 = 2 * convbase10 + (b[countb] - '0');
 	}
-	return (convert);
+	return (convbase10);
 }
