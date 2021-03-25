aed0369673a1f650ee3519de4f3be336806de8b2
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-25 14:30:54 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,15 +5,15 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int convert = 0;
-unsigned int countb;
-if (b == NULL)
-return (0);
-for (countb = 0; b[countb] != '\0'; countb++)
-{
-    if (b[countb] < '0' || b[countb] > '1')
-return (0);
-convert = 2 * convert + (b[countb] - '0');
-}   
-return (convert);        
-}               
+	unsigned int convert = 0;
+	unsigned int countb;
+	if (b == NULL)
+		return (0);
+	for (countb = 0; b[countb] != '\0'; countb++)
+	{
+		if (b[countb] < '0' || b[countb] > '1')
+			return (0);
+		convert = 2 * convert + (b[countb] - '0');
+	}
+	return (convert);
+}
