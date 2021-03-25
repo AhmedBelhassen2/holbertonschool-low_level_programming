3a94642e1b58f4648a606ba9b3cf6c942259bc52
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-25 14:25:42 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,19 @@
-#include holberton.
+#include "holberton.h"
 /**
 *
 *
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int convert;
+unsigned int convert = 0;
 unsigned int countb;
 if (b == NULL)
 return (0);
-for (contb = 0; b[countb] != '\0'; countb++)
+for (countb = 0; b[countb] != '\0'; countb++)
 {
-    if (b[countb < '0' || b(countb) > '1'])
-return (0)
-convert *= 2 + (b[countb] - '0')
+    if (b[countb] < '0' || b[countb] > '1')
+return (0);
+convert = 2 * convert + (b[countb] - '0');
 }   
-return (convert)        
+return (convert);        
 }               
