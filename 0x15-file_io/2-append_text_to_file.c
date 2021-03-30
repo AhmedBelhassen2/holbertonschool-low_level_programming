37267124ea1f934a7ca9add3a965323b08d6868c
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-03-30 22:51:05 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* append_text_to_file - Appends text at a file.
-* @filename: poiner to name
-* @text_content: string
-* Return:1
+* append_text_to_file -  that appends text at the end of a file
+* @filename: the name of the file
+* @text_content: terminated string to add at the end of the file
+* Return: 1 on success and -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -12,8 +12,8 @@ if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len];)
-++len;
+for (len = 0; text_content[len]; ++len)
+;
 }
 op = open(filename, O_WRONLY | O_APPEND);
 wr = write(op, text_content, len);
