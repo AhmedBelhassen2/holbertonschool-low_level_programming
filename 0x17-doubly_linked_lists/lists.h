2f8a68b4729382dba8650770c7f1ef2b7e40cfdb
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-02 20:28:30 +0000

    lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -21,13 +21,13 @@ typedef struct dlistint_s
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-void free_dlistint2(dlistint_t **head);
-int pop_dlistint(dlistint_t **head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *add_dnodeint(dlistint_t **h, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **h, const int n);
+void free_dlistint(dlistint_t *h);
+void free_dlistint2(dlistint_t **h);
+int pop_dlistint(dlistint_t **h);
+dlistint_t *get_dnodeint_at_index(dlistint_t *h, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **h, unsigned int index);
 #endif
