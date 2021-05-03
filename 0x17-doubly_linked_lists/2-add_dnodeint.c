79d20e9536825c4a7f67a720641f967bc657c110
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-03 14:04:32 +0000

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,8 @@ dlistint_t *add_dnodeint(dlistint_t **h, const int n)
 	new_dnodeint_begin->n = n;
 	new_dnodeint_begin->next = *h;
 	new_dnodeint_begin->prev = NULL;
-	*h->prev = new_dnodeint_begin;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*h = new_dnodeint_begin;
 	return (new_dnodeint_begin);
 }
