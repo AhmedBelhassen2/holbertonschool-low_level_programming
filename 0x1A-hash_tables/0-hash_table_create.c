5679c8d71b382972fab8b2f3f6376c517f320f18
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-28 14:58:28 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hashtable == NULL)
 		return (NULL);
 	hashtable->size = size;
-	hashtable->array = malloc(sizeof(hash_node_t*)* size);
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	for (i = 0; i < size; i++)
 		hashtable->array[i] = NULL;
 	return (hashtable);
