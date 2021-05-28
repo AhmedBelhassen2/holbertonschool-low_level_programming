538245e245709bd4e8a8fb9a7f0391df1b917240
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-28 22:56:26 +0000

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ *hash_table_delete - delete the hash table
+ *@ht: the hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *hash = ht;
+	hash_node_t *node, *temp;
+	unsigned long int j;
+
+	for (j = 0; j < ht->size; j++)
+	{
+		if (ht->array[j] != NULL)
+		{
+			node = ht->array[j];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(hash->array);
+	free(hash);
+}
