5828f8fd6a879fa07de9392ea6eab329de85d694
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-07-02 23:20:53 +0000

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,10 +9,10 @@ def island_perimeter(grid):
     :type grid: List[List[int]]
     :rtype: int
     """
-    
+
     land = 0
     edge = 0
-        
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
